456de913a0830e5a3d56338db326576f920b0a2b
soi37 <soi37kb@gmail.com>
2022-09-08 14:53:32 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+int main(void)
+{
+         char c;
+         int i;
+         long;
+         long long lli;
+         float f;
+         printf("Size of a char: %d bytes(s)\n", sizeof(c));
+         printf("Size of an int: %d byte(s)\n", sizeof(i));
+         printf("Size of a long int: %d byte(s)\n", sizeof (l));
+         printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+         return(0);
+}
